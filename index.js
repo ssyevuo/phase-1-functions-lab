d51a0cecca2c725c4df68f345fda8dc94bf13e71
ssyevuo <shakirasyevuo@gmail.com>
2024-11-30 00:17:28 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+    const hq = 42; 
+    return Math.abs(someValue - hq);
+  }
+
+  function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    return blocks * 264;    
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled = Math.abs(destination - start); 
+    return blocksTravelled * 264;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination); 
+  
+    if (distance <= 400) {
+      return 0; 
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02; 
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25; 
+    } else {
+      return 'cannot travel that far'; 
+    }
+  }
+
+
